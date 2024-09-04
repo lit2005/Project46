510d3ae6408622443d534f46df51fd99d4563064
litvinenko0s <litvinenkosasa462@gmail.com>
2024-09-04 13:31:00 +0300

    А1

--- Project46/Source.cpp
+++ Project46/Source.cpp
@@ -1,13 +1,15 @@
 #include <iostream>
 using namespace std;
-void F1(){}
+void F1() {}
 
-void F2(){}
+void F2() {}
 
-void F3(){}
+void F3() {}
+void InitializeArrey() {}
+
+
 
 int main()
 {
- 
-}
 
+}
